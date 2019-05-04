084c8e9432ef03d51e23ca61f7a59a2272950f8e
angad <singh21.h@gmail.com>
2019-05-04 15:32:46 -0400

    commit2

--- Program.cs
+++ Program.cs
@@ -3,13 +3,63 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Net.Sockets;
+using System.Net;
 
 namespace userver
 {
     class Program
     {
         static void Main(string[] args)
+                                         
         {
+
+            if (args.Length > 1) throw new ArgumentException("Usage: userver [<PORT>]");
+
+            int servPort = Int32.Parse(args[0]);
+
+            UdpClient client = null;
+
+            try
+            {
+
+                client = new UdpClient(servPort);
+
+
+            }
+
+            catch (SocketException e)
+            {
+
+                Console.WriteLine(e.Message);
+                Environment.Exit(e.ErrorCode);
+
+            }
+
+            IPEndPoint endpoint = new IPEndPoint(IPAddress.Any, servPort);
+
+            for (; ; ) {
+
+                try
+                {
+
+                    byte[] buffer = client.Receive(ref endpoint);
+
+                    Console.WriteLine("Handling at :" + endpoint.Address + " : " +Encoding.ASCII.GetString(buffer));
+
+                    client.Send(buffer,buffer.Length, endpoint);
+
+                    Console.WriteLine("Echoed {0} bytes", buffer.Length);
+
+                }
+
+                catch (SocketException e)
+                {
+
+                    Console.WriteLine(e.ErrorCode);
+
+                }
+            }         
         }
     }
 }
